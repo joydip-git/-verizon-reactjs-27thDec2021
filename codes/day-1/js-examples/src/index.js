05cd7885f78a0dcb20e62c3d9f81ac80969f65ab
siemens-c2c <joy_mon78@yahoo.co.in>
2021-12-27 12:00:45 +0530

    Update index.js

--- codes/day-1/js-examples/src/index.js
+++ codes/day-1/js-examples/src/index.js
@@ -1,10 +1,27 @@
+//constructor function
 function person(name, id, salary) {
+    //value properties
     this.personName = name
     this.personId = id
     this.personSalary = salary
+
+    //functional property
+    // this.print = function () {
+    //     return this.personName + ' ' + this.personId + ' ' + this.personSalary
+    // }
 }
+person.prototype.print = function () {
+    return this.personName + ' ' + this.personId + ' ' + this.personSalary
+}
+console.log(person.prototype)
+
 const anilPerson = new person('anil', 1, 1000)
 const sunilPerson = new person('sunil', 2, 2000)
 
-console.log(anilPerson)
-console.log(sunilPerson)
+//accessing value properties
+console.log(anilPerson.personName)
+console.log(sunilPerson['personSalary'])
+
+//accessing functional properties
+console.log(anilPerson.print())
+console.log(sunilPerson.print())

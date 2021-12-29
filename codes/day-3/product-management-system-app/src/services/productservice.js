db35522e5d69e1c063307db19feec37d88b62f4a
siemens-c2c <joy_mon78@yahoo.co.in>
2021-12-29 11:38:58 +0530

    pms app created again

---
+++ codes/day-3/product-management-system-app/src/services/productservice.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export function getProducts() {
+    axios
+}
+
+export function getProductById(pid) {
+
+}
+
+export function deleteProductById(pid) {
+
+}
+
+export function addproduct(productObject) {
+
+}
+
+export function addproduct(productObject, pid) {
+
+}
